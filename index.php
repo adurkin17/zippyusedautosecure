6131e7e01e5620ce901492b846b0f967964a222b
Austin Durkin <69984198+adurkin17@users.noreply.github.com>
2022-03-23 22:09:22 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
     require('model/make_db.php');
 
     // Get required data from Model
-    $types = typeDB::get_types();
-    $classes = DBClass::get_classes();
-    $makes = makeClass::get_makes();
+    $types = get_types();
+    $classes = get_classes();
+    $makes = get_makes();
 
     // Get Parameter data sent to Controller
     $action = filter_input(INPUT_GET,'action', FILTER_SANITIZE_STRING);
@@ -37,7 +37,7 @@
     } */
 
     // Extra credit solution 
-    $vehicles = vehicleDB::get_all_vehicles($sort);
+    $vehicles = get_all_vehicles($sort);
     if ($make_id) {
         $make_name = makeClass::get_make_name($make_id);
         $vehicles = array_filter($vehicles, function($array) use ($make_name) {
@@ -45,13 +45,13 @@
         });
     }
     if ($type_id) {
-        $type_name = typeDB::get_type_name($type_id);
+        $type_name = get_type_name($type_id);
         $vehicles = array_filter($vehicles, function($array) use ($type_name) {
             return $array["Type"] === $type_name;
         });
     }
     if ($class_id) {
-        $class_name = DBClass::get_class_name($class_id);
+        $class_name = get_class_name($class_id);
         $vehicles = array_filter($vehicles, function($array) use ($class_name) {
             return $array["Class"] === $class_name;
         });
