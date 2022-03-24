5ea76255542f643c8261b12b501d99e77e428496
Austin Durkin <69984198+adurkin17@users.noreply.github.com>
2022-03-23 21:57:43 -0500

    Update header.php

--- view/header.php
+++ view/header.php
@@ -11,18 +11,5 @@
 <body>
     <main>
         <header>
-            <h1>Zippy Used Autos</h1>
-            <div class = "register">
-
-                <?php if (!isset($_SESSION['userid']) && $action !== 'register') { ?>
-                    <a href= ".?action=register">Register </a>
-
-                <?php } else if (isset($_SESSION['userid']) && $action !== 'register' && $action !== 'logout') { 
-                    $userID = $_SESSION['userid'];
-                    ?>
-
-                    <p> Welcome <?php $userID ?>! <a href = ".?action=logout">Sign Out </a></p>
-                <?php } ?>
-                    
-            </div>
+            <h1>Zippy Used Autos</h1>                   
         </header>
